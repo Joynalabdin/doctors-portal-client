45e1286896638ffe3a85b16268c4a81d28effbda
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2021-11-28 15:23:55 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h1>Home</h1>
+
+        </div>
+    );
+};
+
+export default Home;
